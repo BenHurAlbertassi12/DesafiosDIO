12ccc73d0fa99e1d94a6102dd6b45727cbf8e40f
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-08-21 10:50:08 -0300

    Equilibrando o Saldo

---
+++ JavaScript/Basico/Equilibrando o Saldo/equilibrandoOSaldo.js
@@ -0,0 +1,12 @@
+//Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
+//- "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
+//- "print": imprime um texto de saída (output), pulando linha.
+
+const saldoAtual = parseFloat(gets());
+const valorDeposito = parseFloat(gets());
+const valorRetirada = parseFloat(gets());
+
+const saldoAtualizado = saldoAtual + valorDeposito - valorRetirada;
+
+// Imprime o saldo atualizado com uma casa decimal.
+print('Saldo atualizado na conta:', saldoAtualizado.toFixed(1));
